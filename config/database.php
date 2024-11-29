25edf3a66eaa4e99ec948df4a8222bcee854549d
Klebert Gabriel Hernandez Tello <s21030152@itsch.edu.mx>
2024-11-29 06:25:23 -0600

    COMPLETO 3,0 DB 1

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
                 'port' => env('DB_PORT', '3306'),
                 'database' => env('DB_DATABASE', 'railway'),
                 'username' => env('DB_USERNAME', 'root'),
-                'password' => env('DB_PASSWORD', ''),
+                'password' => env('DB_PASSWORD', 'fnJZUAtyoanhvFePprCShYZzESzciMgv'),
                 'unix_socket' => '',
                 'charset' => 'utf8mb4',
                 'collation' => 'utf8mb4_unicode_ci',
