b95bc877e93d773e915aefac27034204522f5f64
Klebert Gabriel Hernandez Tello <s21030152@itsch.edu.mx>
2024-11-29 06:04:45 -0600

    Completo BD 2.0

--- config/database.php
+++ config/database.php
@@ -42,27 +42,27 @@ return [
             'synchronous' => null,
         ],
 
-        'mysql' => [
-            'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'options' => [
-            PDO::ATTR_TIMEOUT => 5, // Timeout de 5 segundos
+        'connections' => [
+            'mysql' => [
+                'driver' => 'mysql',
+                'url' => env('DB_URL'),
+                'host' => env('DB_HOST', 'containers-us-west-123.railway.app'),
+                'port' => env('DB_PORT', 'PUERTO_ESPECÍFICO'),
+                'database' => env('DB_DATABASE', 'railway'),
+                'username' => env('DB_USERNAME', 'root'),
+                'password' => env('DB_PASSWORD', 'tu_contraseña'),
+                'unix_socket' => '',
+                'charset' => 'utf8mb4',
+                'collation' => 'utf8mb4_unicode_ci',
+                'prefix' => '',
+                'prefix_indexes' => true,
+                'strict' => true,
+                'engine' => null,
+                'options' => [
+                    PDO::ATTR_TIMEOUT => 10,
+                    PDO::MYSQL_ATTR_SSL_CA => null,
+                ],
             ],
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => env('DB_CHARSET', 'utf8mb4'),
-            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
         ],
 
         'mariadb' => [
@@ -150,7 +150,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
